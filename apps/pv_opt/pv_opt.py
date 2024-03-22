9bbdb61270c41d648cd50520c810938f2590f1ab
fboundy <foboundy@gmail.com>
2024-03-22 17:44:51 +0000

    4.0.0-alpha-6

--- apps/pv_opt/pv_opt.py
+++ apps/pv_opt/pv_opt.py
@@ -18,7 +18,7 @@ OCTOPUS_PRODUCT_URL = r"https://api.octopus.energy/v1/products/"
 
 USE_TARIFF = True
 
-VERSION = "4.0.0-alpha-5"
+VERSION = "4.0.0-alpha-6"
 DEBUG = False
 
 DATE_TIME_FORMAT_LONG = "%Y-%m-%d %H:%M:%S%z"
