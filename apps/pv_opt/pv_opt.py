6b08d0f7246c310424cc5b56f0bb2fd7d077a566
fboundy <foboundy@gmail.com>
2024-03-25 10:56:25 +0000

    solax-x1-8

--- apps/pv_opt/pv_opt.py
+++ apps/pv_opt/pv_opt.py
@@ -18,7 +18,7 @@ OCTOPUS_PRODUCT_URL = r"https://api.octopus.energy/v1/products/"
 
 USE_TARIFF = True
 
-VERSION = "4.0.0-alpha-7"
+VERSION = "4.0.0-solax-x1-8"
 DEBUG = False
 
 DATE_TIME_FORMAT_LONG = "%Y-%m-%d %H:%M:%S%z"
