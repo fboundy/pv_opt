0fa4b71057db86b9f686b1691946ba395e10655a
fboundy <foboundy@gmail.com>
2024-03-25 12:37:15 +0000

    Add entities

--- apps/pv_opt/solax.py
+++ apps/pv_opt/solax.py
@@ -4,27 +4,6 @@ import time
 TIMEFORMAT = "%H:%M"
 INVERTER_DEFS = {
     "SOLAX_X1": {
-        # "modes": {
-        #     1: "Selfuse - No Grid Charging",
-        #     3: "Timed Charge/Discharge - No Grid Charging",
-        #     17: "Backup/Reserve - No Grid Charging",
-        #     33: "Selfuse",
-        #     35: "Timed Charge/Discharge",
-        #     37: "Off-Grid Mode",
-        #     41: "Battery Awaken",
-        #     43: "Battery Awaken + Timed Charge/Discharge",
-        #     49: "Backup/Reserve - No Timed Charge/Discharge",
-        #     51: "Backup/Reserve",
-        # },
-        # "bits": [
-        #     "SelfUse",
-        #     "Timed",
-        #     "OffGrid",
-        #     "BatteryWake",
-        #     "Backup",
-        #     "GridCharge",
-        #     "FeedInPriority",
-        # ],
         # Default Configuration: Exposed as inverter.config and provides defaults for this inverter for the
         # required config. These config items can be over-written by config specified in the config.yaml
         # file. They are required for the main PV_Opt module and if they cannot be found an ERROR will be
@@ -42,6 +21,18 @@ INVERTER_DEFS = {
         # from the config.yaml file but not rquired outside of this module
         "brand_config": {
             "battery_voltage": 100,
+            "id_allow_grid_charge": "select.{device_name}_allow_grid_charge",
+            "id_battery_capacity": "sensor.{device_name}_battery_capacity",
+            "id_battery_install_capacity": "sensor.{device_name}_battery_install_capacity",
+            "id_battery_minimum_capacity": "number.{device_name}_battery_minimum_capacity",
+            "id_battery_type": "sensor.{device_name}_battery_type",
+            "id_battery_charge_max_current": "number.{device_name}_battery_charge_max_current",
+            "id_battery_discharge_max_current": "number.{device_name}_battery_discharge_max_current",
+            "id_charge_end_time_1": "select.{device_name}_charge_end_time_1",
+            "id_charge_start_time_1": "select.{device_name}_charge_start_time_1",
+            "id_use_mode": "select.{device_name}_use_mode",
+            "id_export_duration": "select.{device_name}_export_duration",
+            "id_forcetime_period_1_max_capacity": "number.{device_name}_forcetime_period_1_max_capacity",
         },
     },
 }
@@ -86,32 +77,7 @@ class InverterController:
 
     def hold_soc(self, enable, soc=None):
         if self.type == "SOLAX_X1":
-            if enable:
-                self._solis_set_mode_switch(SelfUse=True, Timed=False, GridCharge=True, Backup=True)
-            else:
-                self.enable_timed_mode()
-
-            # Waiyt for a second to make sure the mode is correct
-            time.sleep(1)
-
-            if soc is None:
-                soc = self.host.get_config("maximum_dod_percent")
-
-            entity_id = self.host.config["id_backup_mode_soc"]
-
-            self.log(f"Setting Backup SOC to {soc}%")
-            if self.type == "SOLIS_SOLAX_MODBUS":
-                changed, written = self._write_and_poll_value(entity_id=entity_id, value=soc)
-            elif self.type == "SOLIS_CORE_MODBUS" or self.type == "SOLIS_SOLARMAN":
-                changed, written = self.solis_write_holding_register(
-                    address=INVERTER_DEFS(self.type)["registers"]["backup_mode_soc"],
-                    value=soc,
-                    entity_id=entity_id,
-                )
-            else:
-                e = "Unknown inverter type"
-                self.log(e, level="ERROR")
-                raise Exception(e)
+            pass
 
     @property
     def status(self):
