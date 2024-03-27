c63664b743ae861a881d19ca270206f6bd557be9
fboundy <foboundy@gmail.com>
2024-03-27 16:23:43 +0000

    3.13.0-ev-beta-2

--- apps/pv_opt/pv_opt.py
+++ apps/pv_opt/pv_opt.py
@@ -18,7 +18,7 @@ OCTOPUS_PRODUCT_URL = r"https://api.octopus.energy/v1/products/"
 
 USE_TARIFF = True
 
-VERSION = "3.13.0-ev-beta-1"
+VERSION = "3.13.0-ev-beta-2"
 DEBUG = False
 
 DATE_TIME_FORMAT_LONG = "%Y-%m-%d %H:%M:%S%z"
