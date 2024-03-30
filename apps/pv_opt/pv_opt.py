7b5ea28f8bc035db05d43475bb9d49a9414cfd4b
fboundy <foboundy@gmail.com>
2024-03-30 13:30:11 +0000

    alpha-13

--- apps/pv_opt/pv_opt.py
+++ apps/pv_opt/pv_opt.py
@@ -12,7 +12,7 @@ import numpy as np
 from numpy import nan
 import re
 
-VERSION = "4.0.0-sunsynk-alpha-12"
+VERSION = "4.0.0-sunsynk-alpha-13"
 
 OCTOPUS_PRODUCT_URL = r"https://api.octopus.energy/v1/products/"
 
