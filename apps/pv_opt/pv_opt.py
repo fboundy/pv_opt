c1a2530f3d658762e867c253b45cafd493e6a406
fboundy <foboundy@gmail.com>
2025-01-26 10:52:00 +0000

    Fix typo

--- apps/pv_opt/pv_opt.py
+++ apps/pv_opt/pv_opt.py
@@ -2317,7 +2317,7 @@ class PVOpt(hass.Hass):
 
             self.io_prices = self.get_io_tariffs(self.octopus_import_entity[0])
 
-        elif ((pd.Timestamp.now(tz="UTC") - self.contract_last_loaded).total_seconds / 3600) > 6:
+        elif ((pd.Timestamp.now(tz="UTC") - self.contract_last_loaded).total_seconds() / 3600) > 6:
             # Reload every 6 hours
             self._load_contract()
 
